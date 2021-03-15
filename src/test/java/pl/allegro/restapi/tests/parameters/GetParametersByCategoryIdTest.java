0b3a34a5130740cd582a01c5e6e4291b806d6eb3
rszwast <rafal.szwast@gmail.com>
2021-03-15 18:38:36 +0100

    test: added GetParametersByCategoryIdTest

--- src/test/java/pl/allegro/restapi/tests/parameters/GetParametersByCategoryIdTest.java
+++ src/test/java/pl/allegro/restapi/tests/parameters/GetParametersByCategoryIdTest.java
@@ -1,4 +1,51 @@
 package pl.allegro.restapi.tests.parameters;
 
-public class GetParametersByCategoryIdTest {
+import org.apache.http.HttpStatus;
+import org.assertj.core.api.Assertions;
+import org.testng.annotations.Test;
+import pl.allegro.restapi.main.pojo.categories.Categories;
+import pl.allegro.restapi.main.pojo.categories.Category;
+import pl.allegro.restapi.main.pojo.parameters.Parameter;
+import pl.allegro.restapi.main.pojo.parameters.Parameters;
+import pl.allegro.restapi.tests.testbases.TestBase;
+
+import java.util.List;
+import java.util.Random;
+
+import static io.restassured.RestAssured.given;
+
+
+public class GetParametersByCategoryIdTest extends TestBase {
+
+    @Test
+    public void givenCategoryIdWhenGetParametersThenReturnListOfParameters() {
+        Categories categories = given()
+                .when().get(environmentConfig.salePath() + "/categories")
+                .then().statusCode(HttpStatus.SC_OK).extract().as(Categories.class);
+
+        List<Category> listOfCategories = categories.getCategories();
+
+        int sizeOfCategories = listOfCategories.size();
+
+        Random random = new Random();
+        Category category = listOfCategories.get(random.nextInt(sizeOfCategories));
+
+        String categoryId = category.getId();
+
+        Parameters parameters = given()
+                .when().get(environmentConfig.salePath() + "/categories/{categoryId}/parameters", categoryId)
+                .then().statusCode(HttpStatus.SC_OK).extract().as(Parameters.class);
+
+        List<Parameter> listOfParameters = parameters.getParameters();
+
+        int sizeOfParameters = listOfParameters.size();
+
+        for (int i = 0; i < sizeOfParameters; i++) {
+            String parameterType = listOfParameters.get(i).getType();
+            System.out.println(parameterType);
+            Assertions.assertThat(parameterType).isNotEmpty();
+        }
+
+    }
+
 }
