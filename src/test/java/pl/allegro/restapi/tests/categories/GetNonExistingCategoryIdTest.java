fda2c60d312d9bce05a10e65f787297352c2e0e9
rszwast <rafal.szwast@gmail.com>
2021-03-16 12:29:14 +0100

    reafctor: GetNonExistingCategoryIdTest

--- src/test/java/pl/allegro/restapi/tests/categories/GetNonExistingCategoryIdTest.java
+++ src/test/java/pl/allegro/restapi/tests/categories/GetNonExistingCategoryIdTest.java
@@ -3,11 +3,15 @@ package pl.allegro.restapi.tests.categories;
 import org.apache.http.HttpStatus;
 import org.assertj.core.api.Assertions;
 import org.testng.annotations.Test;
+import pl.allegro.restapi.main.pojo.categories.Categories;
+import pl.allegro.restapi.main.pojo.categories.Category;
 import pl.allegro.restapi.main.pojo.errors.Error;
 import pl.allegro.restapi.tests.testbases.TestBase;
 import pl.allegro.restapi.main.pojo.errors.Errors;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import static io.restassured.RestAssured.given;
 
@@ -15,8 +19,34 @@ public class GetNonExistingCategoryIdTest extends TestBase {
 
     @Test
     public void givenNonExistingCategoryWhenGetCategoryThenNotFoundReturn(){
+        Categories categories = given()
+                .when().get(environmentConfig.salePath() + "/categories")
+                .then().statusCode(HttpStatus.SC_OK).extract().as(Categories.class);
+
+        List<Category> listOfCategories = categories.getCategories();
+
+        List<String> listOfCategoryIds = new ArrayList<String>() {
+        };
+
+        for (Category category : listOfCategories){
+            String categoryId = category.getId();
+            listOfCategoryIds.add(categoryId);
+        }
+
+        System.out.println(listOfCategoryIds);
+
+        Random random = new Random();
+        String nonExistingCategoryId;
+
+        do {
+            nonExistingCategoryId = Integer.toString(random.nextInt());
+            System.out.println("Non existing category: " + nonExistingCategoryId);
+        } while (listOfCategoryIds.contains(nonExistingCategoryId));
+
+        System.out.println("Non existing: " + nonExistingCategoryId);
+
         Errors errors = given()
-                .when().get(environmentConfig.salePath() + "categories/{categoryId}", "6061")
+                .when().get(environmentConfig.salePath() + "categories/{categoryId}", nonExistingCategoryId)
                 .then().statusCode(HttpStatus.SC_NOT_FOUND).extract().as(Errors.class);
 
         List<Error> errorsList = errors.getErrors();
